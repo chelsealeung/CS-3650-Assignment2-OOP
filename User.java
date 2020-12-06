62bf16057772744a67ea091849fb1ab679dc322f
Chelsea Leung <leungchelsea2018@gmail.com>
2020-12-06 00:30:55 -0800

    Update User.java

--- User.java
+++ User.java
@@ -2,6 +2,7 @@ package sample;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,6 +10,8 @@ import java.util.List;
 public class User extends Subject implements Observer, SysEntry {
 
     private String userID;
+    private Long creationTime;  // Assignment 3
+    private Long lastUpdateTime;    // Assignment 3
     private List<String> totalTweet = new ArrayList<String>();  // used for MessageTotal and PositivePercentage
     // ObservableList: A list that allows listeners to track changes when they occur
     private List<User> following = new ArrayList<User>();
@@ -38,15 +41,34 @@ public class User extends Subject implements Observer, SysEntry {
         visitor.visitUser(this);
     }
 
+    @Override
+    public Long getCreationTime() {
+        return this.creationTime;
+    }
+
+    @Override
+    public void setCreationTime() {
+        this.creationTime = System.currentTimeMillis();
+    }
+
+    public Long getLastUpdateTime() {
+        return this.lastUpdateTime;
+    }
+
+    public void setLastUpdateTime() {
+        this.lastUpdateTime = System.currentTimeMillis();
+    }
+
+
     public List<String> getTweet() {
         return totalTweet;
     }
 
     // Observer
     @Override
-    public void update(Subject subject, String tweet) {
+    public void update(Subject subject, String tweet, Long lastUpdateTime) {
         if (subject instanceof User) {
-            newsFeedObservable.add(((User) subject).getID() + ": " + tweet);
+            newsFeedObservable.add("[" + lastUpdateTime + "] " + ((User) subject).getID() + ": " + tweet);
         }
     }
 
@@ -70,10 +92,10 @@ public class User extends Subject implements Observer, SysEntry {
         return newsFeedObservable;
     }
 
-    public void setNewsFeedObservable(String tweet) {
+    public void setNewsFeedObservable(String tweet, Long lastUpdateTime) {
         totalTweet.add(tweet);
-        newsFeedObservable.add(userID + ": " + tweet);
-        notifyObservers(tweet);
+        newsFeedObservable.add("[" + lastUpdateTime + "] " + userID + ": " + tweet);
+        notifyObservers(tweet, lastUpdateTime);
     }
 
 }
